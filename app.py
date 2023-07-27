af7820aa2945bd55752d0b4a91675bdb4e9f67da
bk7312 <bk7312@users.noreply.github.com>
2023-07-27 20:44:06 +0800

    added 404, minor text edits

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from datetime import datetime
-from pytz import timezone
 
 app = Flask(__name__)
 
@@ -14,7 +12,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-# @app.after_request
+@app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -26,10 +24,10 @@ def after_request(response):
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        print('post')
         if request.form.get("reset") == "reset":
             session.clear()
         return render_template("index.html")
+    
     else:
         return render_template("index.html")
 
@@ -37,17 +35,13 @@ def index():
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
-        print('post')
         if not request.form.get("username"):
-            return render_template("login.html", message="Must enter a valid username/password!")
+            return render_template("login.html", message="Must enter a valid username!")
         
         username = request.form.get("username")
 
         if session.get("user_created") and session["username"] == username and not request.form.get("password"):
-            return render_template("win.html", message="Congrats, you managed to log in!")
-
-        if not request.form.get("password"):
-            return render_template("login.html", message="Must enter a valid username/password!")
+            return render_template("win.html", message="Congrats, you have successfully logged in!")
                 
         return render_template("login.html", message="Must enter a valid username/password!")
 
@@ -55,7 +49,8 @@ def login():
         if session.get("user_created"):
             message = "Congrats on creating a username and password! You still remember it right?"
         else:
-            message = "Do you have a username and password?"
+            message = "Don't have a username and password? Find a way to register!"
+
         return render_template("login.html", message=message)
 
 
@@ -64,56 +59,69 @@ def register():
     if request.method == "POST":
         if not request.form.get("validate"):
             return render_template("register.html", message="Looks like something went wrong, let's start over!")
+        
         type = request.form.get("validate").lower()
+
 
         match type:
             case "username":
-                print("username")
                 if not request.form.get("username"):
                     return render_template("register.html", message="Must enter a valid username!")
+                
                 username = request.form.get("username")
+
                 if len(username) == 16:
                     session["username"] = username
-                    return render_template("register_pwd.html", message="Congrats on getting that username")
+                    return render_template("register_pwd.html", message="Congrats on getting that username!")
+                
                 return render_template("register.html", message="Username must be exactly 16 characters long!")
             
+
             case "password":
-                print("pwd")
                 if not request.form.get("password") and not request.form.get("confirm"):
                     return render_template("register_survey.html", message="Congrats on entering an empty password!")
-                return render_template("register_pwd.html", message="Please submit a blank password!")
+                
+                return render_template("register_pwd.html", message="Please find a way to submit a blank password!")
             
+
             case "survey":
-                print("s")
                 if not request.form.get("survey"):
                     return render_template("register_survey.html", message="Please choose a color!")
+                
                 color = request.form.get("survey")
+
                 if color.lower() == 'pink':
                     return render_template("register_em.html", message="Glad to know you like pink!")
+                
                 return render_template("register_survey.html", message="Please select pink!")
             
+
             case "email":
-                print("em")
                 if not request.form.get("email"):
                     return render_template("register_em.html", message="Please enter your email!")
-                em = request.form.get("email")
-                if em == '#me&email*net':
+                
+                email = request.form.get("email")
+
+                if email == '#me&email*net':
                     return render_template("register_disagree.html", message="What a nice email address!")
+                
                 return render_template("register_em.html", message="Please enter the correct email!")
             
+
             case "agree":
-                print("a")
                 return render_template("register_disagree.html", message="Please disagree!")
 
+
             case "disagree":
-                print("d")
                 if not request.form.get("terms"):
                     return render_template("register_disagree.html", message="Please check the box!")
+                
                 session["user_created"] = True
+
                 return render_template("register_ok.html", message="Congrats on registering!")
 
+
             case _:
-                print("catchall")
                 return render_template("register.html", message="Looks like something went wrong, let's start over!")
         
     else:
@@ -124,3 +132,6 @@ def register():
 def about():
     return render_template("about.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
