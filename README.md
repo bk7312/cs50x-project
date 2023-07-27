ecb6d9eca813348132d14058f6dc2f3291bf3f00
bk7312 <bk7312@users.noreply.github.com>
2023-07-27 17:13:18 +0800

    initial app

---
+++ README.md
@@ -0,0 +1,27 @@
+# YOUR PROJECT TITLE
+#### Video Demo:  <URL HERE>
+#### Description:
+TODO
+
+---
+
+What will your software do? 
+- Challenge the user to login, educate on client vs server side form validation, teach dev tools
+
+What features will it have?
+- Client side input validation to prevent users from creating an account or logging in
+
+How will it be executed?
+- Using python/flask on the backend
+
+What new skills will you need to acquire? What topics will you need to research?
+- Handling form inputs, client side vs server side input validation
+
+In the world of software, most everything takes longer to implement than you expect. And so it’s not uncommon to accomplish less in a fixed amount of time than you hope. What might you consider to be a good outcome for your project? A better outcome? The best outcome?
+- Basic registration page to register followed by a login page to login
+
+---
+
+Notes:
+activate the python environment in terminal $ . .venv/bin/activate
+open command palette and select python: select interpreter to change to .env environment
---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from datetime import datetime
+from pytz import timezone
+
+app = Flask(__name__)
+
+# activate the environment
+# terminal $ . .venv/bin/activate
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# @app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        print('post')
+        if request.form.get("reset") == "reset":
+            session.clear()
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        print('post')
+        if not request.form.get("username"):
+            return render_template("login.html", message="Must enter a valid username/password!")
+        
+        username = request.form.get("username")
+
+        if session.get("user_created") and session["username"] == username and not request.form.get("password"):
+            return render_template("win.html", message="Congrats, you managed to log in!")
+
+        if not request.form.get("password"):
+            return render_template("login.html", message="Must enter a valid username/password!")
+                
+        return render_template("login.html", message="Must enter a valid username/password!")
+
+    else:
+        if session.get("user_created"):
+            message = "Congrats on creating a username and password! You still remember it right?"
+        else:
+            message = "Do you have a username and password?"
+        return render_template("login.html", message=message)
+
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        if not request.form.get("validate"):
+            return render_template("register.html", message="Looks like something went wrong, let's start over!")
+        type = request.form.get("validate").lower()
+
+        match type:
+            case "username":
+                print("username")
+                if not request.form.get("username"):
+                    return render_template("register.html", message="Must enter a valid username!")
+                username = request.form.get("username")
+                if len(username) == 16:
+                    session["username"] = username
+                    return render_template("register_pwd.html", message="Congrats on getting that username")
+                return render_template("register.html", message="Username must be exactly 16 characters long!")
+            
+            case "password":
+                print("pwd")
+                if not request.form.get("password") and not request.form.get("confirm"):
+                    return render_template("register_survey.html", message="Congrats on entering an empty password!")
+                return render_template("register_pwd.html", message="Please submit a blank password!")
+            
+            case "survey":
+                print("s")
+                if not request.form.get("survey"):
+                    return render_template("register_survey.html", message="Please choose a color!")
+                color = request.form.get("survey")
+                if color.lower() == 'pink':
+                    return render_template("register_em.html", message="Glad to know you like pink!")
+                return render_template("register_survey.html", message="Please select pink!")
+            
+            case "email":
+                print("em")
+                if not request.form.get("email"):
+                    return render_template("register_em.html", message="Please enter your email!")
+                em = request.form.get("email")
+                if em == '#me&email*net':
+                    return render_template("register_disagree.html", message="What a nice email address!")
+                return render_template("register_em.html", message="Please enter the correct email!")
+            
+            case "agree":
+                print("a")
+                return render_template("register_disagree.html", message="Please disagree!")
+
+            case "disagree":
+                print("d")
+                if not request.form.get("terms"):
+                    return render_template("register_disagree.html", message="Please check the box!")
+                session["user_created"] = True
+                return render_template("register_ok.html", message="Congrats on registering!")
+
+            case _:
+                print("catchall")
+                return render_template("register.html", message="Looks like something went wrong, let's start over!")
+        
+    else:
+        return render_template("register.html", message="Congrats on reaching the registration page!")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
